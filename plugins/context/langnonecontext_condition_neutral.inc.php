b0941f729bbf6211d2685c7e83d4e07f4aa71c2d
Masa Sakano <masasakano@users.noreply.github.com>
2014-10-08 01:53:23 +0100

    First release, after tidying up the codes.

--- plugins/context/langnonecontext_condition_neutral.inc.php
+++ plugins/context/langnonecontext_condition_neutral.inc.php
@@ -1,36 +1,47 @@
 <?php
 /**
- * Expose Web Area Contact Form as a Context condition.
+ * Add whether the language is neutral or not as a Context condition.
  *
  * cf.:
  *  - https://ohthehugemanatee.org/blog/2013/12/02/custom-context-conditions/
  *  - http://www.phase2technology.com/blog/the-joy-of-extending-context/
+ *  - See http://dtek.net/blog/extending-drupals-context-module-custom-condition-based-field-value
  */
 class langnonecontext_condition_neutral extends context_condition {
+
+  /**
+   * Set the values, as you see (for the Radio-buttons) in the UI of Context.
+   *
+   * Though Radio-buttons is set as the type (see below), the returned variable
+   * is the same as for Checkboxes, that is, each key in the returned array
+   * is either defined or not, technically independent of what the other is,
+   * though a user can not set both to be true (or false) via the UI.
+   */
   function condition_values() {
     return array(
-      'TRUE' => t('Node is Language-Neutral'),
-      'FALSE' => t('Node has a defined language'),
+      'kTRUE'  => t('Node is Language-Neutral'),
+      'kFALSE' => t('Node has a defined language'),
     );
   }
 
+  /**
+   * Set the form in the UI of Context.
+   */
   function condition_form($context) {
     $form = parent::condition_form($context);
     // http://api.drupal.psu.edu/api/drupal/modules!contrib!context!plugins!context_condition.inc/function/context_condition%3A%3Acondition_form/cis7
 
-    $form['#type'] = 'radios';	// 'checkboxes' in default.
+    $form['#type'] = 'radios';
+	// Modifies from the default 'checkboxes'.
+
     if(empty($form['#default_value'])){
-      $form['#default_value'] = 'TRUE';
+      $form['#default_value'] = 'kTRUE';
+      // In default, 'Node is Language-Neutral' is ticked in the UI.
     }
     else{
       $form['#default_value'] = current($form['#default_value']);
       // Makes it the scalar.
     }
-
-    // unset($form['#options']);
-    // // http://www.phase2technology.com/blog/the-joy-of-extending-context/
-    // // sets this for whatever reason...  For '#options' see:
-    // // http://api.drupal.psu.edu/api/drupal/modules!contrib!context!plugins!context_condition.inc/function/context_condition%3A%3Acondition_form/cis7
 
     return $form;
   }
@@ -38,7 +49,7 @@ class langnonecontext_condition_neutral extends context_condition {
   /**
    * Condition form submit handler.
    *
-   * Storing values in an array since that's what Context prefers
+   * Storing values in an array since that's what Context prefers.
    */
   function condition_form_submit($values) {
     return array_filter(array($values => $values));
@@ -46,39 +57,34 @@ class langnonecontext_condition_neutral extends context_condition {
   }
 
 
+  /**
+   * Core function for the context.
+   *
+   * A user can set either "Language-Neutral" or not to satisfy the conditions.
+   * Here returns true if the status of the node for the language matches
+   * the user's request.
+   * If not, nothing is done, as the Context module only checks a TRUE value.
+   *
+   * Note in Drupal 7 (i18n module):
+   *  - if the language is neutral, ($node->tnid == 0),
+   *  - if it is the source node for the language, ($node->tnid == $node->nid),
+   *  - if it is one of translated nodes, $node->tnid is nId of its source.
+   */
   function execute($node) {
-  // function execute($value = NULL) {
-    // // If nothing is passed then use now
-    // if (empty($value)) {
-    //   $value = time();
-    // }
-  // function execute($group) {
-    // if (!empty($group) && $group['group_type'] == 'node') {
-    //   $group_node = entity_load_single($group['group_type'], $group['gid']);
-    //   $group_wrapper = entity_metadata_wrapper('node', $group_node);
-    //   $special_nid = $group_wrapper->field_special_node->value();
 
     foreach ($this->get_contexts() as $context) {
       $values= $this->fetch_from_context($context, 'values');
 
-      if ($node->tnid == 0 && !empty($values['TRUE'])) {
-        // Node is Language-Neutral, and the condition is set to match
-        // Language-Neutral.
+      if     ($node->tnid == 0 && !empty($values['kTRUE'])) {
+        // Node is Language-Neutral, and the condition is set by the user
+        // to match Language-Neutral.
         $this->condition_met($context);
       }
-      if ($node->tnid != 0 && !empty($values['FALSE'])) {
-        // Node is not Language-Neutral, and the condition is set to match
-        // non Language-Neutral, aka the node in a certain language.
+      elseif ($node->tnid != 0 && !empty($values['kFALSE'])) {
+        // Node is not Language-Neutral, and the condition is set by the user
+        // to match non Language-Neutral, aka the node in a certain language.
         $this->condition_met($context);
       }
-
-      // Drupal 7 function arg():
-      //  https://api.drupal.org/api/drupal/includes!bootstrap.inc/function/arg/7
-      // When the current path is /admin/structure/types
-      //   arg(0) => 'admin'
-      //   arg(1) => 'structure'
-      //   arg(2) => 'types'
-    }
-    // }
-  }
+    }	// foreach ($this->get_contexts() as $context) {
+  }	// function execute($node) {
 }
